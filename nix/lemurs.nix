b50cabfe06a1a8f358a2a3c36bc1525ab33bdc37
NullCube <nullcub3@gmail.com>
2024-06-29 19:28:20 -0700

    Temp: add patchfile from auvred
    https://github.com/auvred/nixos-config/blob/main/overlays/lemurs/use-system-shell-in-key-menu.patch

--- nix/lemurs.nix
+++ nix/lemurs.nix
@@ -21,6 +21,10 @@ rustPlatform.buildRustPackage {
 
   cargoHash = "sha256-rJLHfedg4y8cZH77AEA4AjE0TvWf9tdSjKiHZfvW+gw=";
 
+  patches = [
+    ./use-system-shell-in-key-menu.patch
+  ];
+
   meta = with lib; {
     description = "A customizable TUI display/login manager written in Rust";
     homepage = "https://github.com/coastalwhite/lemurs";
